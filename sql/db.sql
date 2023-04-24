665b435a8ca892dfcf9ec21f7b9655e469712036
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-04-24 15:58:23 -0500

    plz add all queries to create tables in the db

---
+++ sql/db.sql
@@ -0,0 +1,14 @@
+-- Add more table as needed. I have added a sample query for creating a table for Users here.
+-- Alex, please finalize how will you design the database.
+-- Guess it will be different from the ER diagram that I have designed.
+
+CREATE TABLE IF NOT EXIStS sys.Users (
+    userID INTEGER,
+    userName VARCHAR(256) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    fullName VARCHAR(1000) NOT NULL,
+    email VARCHAR(1000) NOT NULL,
+    publicKey VARCHAR(1000) NOT NULL,
+    privateKey VARCHAR(1000) NOT NULL,
+    PRIMARY KEY (userID)
+);
