ff7d251c67302447005e615c137c25f5a9876577
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-05-02 08:55:09 -0500

    Add files via upload

---
+++ static/assets/js/view_election_details.js
@@ -0,0 +1,52 @@
+const election_title = document.getElementById("election_ttle");
+const election_prompt = document.getElementById("election_prompt");
+const yes_votes_number = document.getElementById("yes_votes_number");
+const no_votes_number = document.getElementById("no_votes_number");
+
+var pie = document.getElementById('pieChart').getContext('2d');
+var myChart = new Chart(pie, {
+    type: 'pie',
+    data: {
+        labels: ['Yes', 'No'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+var bar = document.getElementById('barChart').getContext('2d');
+var myChart = new Chart(bar, {
+    type: 'bar',
+    data: {
+        labels: ['Yes', 'No'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
