2ae702e2e69e1d5eed86507e3032cc1d5b089157
Alex Richardson <william.a.richardson@vanderbilt.edu>
2023-05-04 00:42:13 -0500

    Created backend configuration.

--- simayiAPI/get_current_nodes.php
+++ simayiAPI/get_current_nodes.php
@@ -5,7 +5,12 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 function get_all_data() {
-  //$all_data = ["a", "b", "c", "SAdfasdf", "sadf"];
+  $result = yaml_parse_file("backend_configuration.yaml");
+  $all_data = array();
+  foreach ($result as $key => $value) {
+    $link = "http://ransom.isis.vanderbilt.edu:".strval($value["port"]);
+    $all_data[] = $link;
+  }
 	$all_data = ['http://ransom.isis.vanderbilt.edu:5000'];
   return $all_data;
 }
